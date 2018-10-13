578bfc148124178d558e11b3483f4e403b55023b
Carlos Villalta <CarlosFindley@protonmail.com>
2018-10-13 02:11:20 -0700

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     // create random number to match between 25 and 135
     var randomNumberGenerator = Math.round(Math.random() * 110) + 25;
         // add the random number to Match This Number text
-        $(".RandomNumberToMatch").html("Match This Number: " + randomNumberGenerator);
+        $(".RandomNumberToMatch").html("&#x25C8; Match This Score: " + randomNumberGenerator + " &#x25C8;");
 
     // create random numbers per gem between 1 and 16 AND add the random number to gems
     var rubyRandomNumber = Math.round(Math.random() * 15) + 1;
@@ -20,6 +20,20 @@ $(document).ready(function() {
         $(".diamondGem").data(rubyRandomNumber);
         console.log("dia", diamondRandomNumber);
 
+    // set Total Score var
+    var totalScore = 0;
+        // add the score value to score text
+        $(".score").html("Your Current Score: " + totalScore);
+
+    // set wins var
+    var totalWins = 0;
+        // add the score value to score text
+        $(".wins").html("Wins: " + totalWins);
+
+    // set losses var
+    var totalLosses = 0;
+        // add the score value to score text
+        $(".losses").html("Losses: " + totalWins);
 
 });
 
